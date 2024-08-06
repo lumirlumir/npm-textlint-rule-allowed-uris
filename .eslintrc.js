502532509e46329edfaf9cdf2c700f71c8113345
루밀LuMir <rpfos@naver.com>
2024-08-07 01:15:56 +0900

    feat: introduction of eslint and prettier
    eslint 및 prettier 도입.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  plugins: [
+    // plugins has no priority.
+    'import',
+    'prettier',
+  ],
+  extends: [
+    // extends has priority. Last index has the highest priority.
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  rules: {},
+  env: {
+    commonjs: true,
+    es2024: true,
+    node: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+};
