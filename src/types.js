6075a6cd83680dee5592ed2c7d1be248cfdb0914
루밀LuMir <rpfos@naver.com>
2025-03-04 01:46:14 +0900

    refactor(*): create `src/types.js` (#174)
    This pull request introduces a new file `src/types.js` to define common
    types used within the codebase. The changes include adding type
    definitions and ensuring type checking with TypeScript.
    
    Key changes:
    
    * Added a file overview comment to `src/types.js` to describe the
    purpose of the file.
    * Enabled TypeScript checking with the `@ts-check` comment.
    * Defined a `Uri` type as a string and a `Type` type as either `'link'`
    or `'image'`.
    * Created a `UriTypeObject` typedef that includes properties `uri` of
    type `Uri` and `type` of type `Type`.
    * Exported the module to make the types available for use in other
    files.

---
+++ src/types.js
@@ -0,0 +1,29 @@
+/**
+ * @fileoverview Define common types.
+ */
+
+// @ts-check
+
+// --------------------------------------------------------------------------------
+// Typedefs
+// --------------------------------------------------------------------------------
+
+/**
+ * @typedef {string} Uri The URI. It should be `string` type.
+ */
+
+/**
+ * @typedef {'link' | 'image'} Type The type of URI. It should be either `'link'` or `'image'`.
+ */
+
+/**
+ * @typedef {object} UriTypeObject
+ * @property {Uri} uri The URI. It should be `string` type.
+ * @property {Type} type The type of URI. It should be either `'link'` or `'image'`.
+ */
+
+// --------------------------------------------------------------------------------
+// Exports
+// --------------------------------------------------------------------------------
+
+module.exports = {};
