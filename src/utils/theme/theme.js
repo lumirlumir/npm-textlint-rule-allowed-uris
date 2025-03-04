2cd6ab203d51fe41615d191bca802a3a63131b77
루밀LuMir <rpfos@naver.com>
2025-03-04 15:22:17 +0900

    refactor(*): restructure `theme` utilities and update usage in files (#176)
    This pull request includes several changes to improve code organization
    and readability in the `src` directory. The most important changes
    include restructuring imports, adding documentation comments, and
    introducing a new theme module for console output.
    
    ### Code organization and readability improvements:
    
    *
    [`src/textlint-rule-allowed-uris.js`](diffhunk://#diff-7da72e1e0d5c49a43b94f9d928d3157918c0bdcc3121c5ed14e0bc459b3f26f9L7-R17):
    Restructured imports by grouping them under headers and replaced
    `ansi-colors` with the new theme module for console output.
    [[1]](diffhunk://#diff-7da72e1e0d5c49a43b94f9d928d3157918c0bdcc3121c5ed14e0bc459b3f26f9L7-R17)
    [[2]](diffhunk://#diff-7da72e1e0d5c49a43b94f9d928d3157918c0bdcc3121c5ed14e0bc459b3f26f9L53-R61)
    [[3]](diffhunk://#diff-7da72e1e0d5c49a43b94f9d928d3157918c0bdcc3121c5ed14e0bc459b3f26f9R71-R74)
    *
    [`src/utils/getDefinitionNodeUriType.js`](diffhunk://#diff-9c07b238e45a785203810889d94abae9a36233ef368a31737760c7823c53f16aR1-R19):
    Added documentation comments, restructured imports, and replaced
    `ansi-colors` with the new theme module for error messages.
    [[1]](diffhunk://#diff-9c07b238e45a785203810889d94abae9a36233ef368a31737760c7823c53f16aR1-R19)
    [[2]](diffhunk://#diff-9c07b238e45a785203810889d94abae9a36233ef368a31737760c7823c53f16aL21-R35)
    [[3]](diffhunk://#diff-9c07b238e45a785203810889d94abae9a36233ef368a31737760c7823c53f16aR44-R47)
    
    ### New theme module:
    
    *
    [`src/utils/theme/index.js`](diffhunk://#diff-d2f139c46cac1980332b8cbbeceb47c1cf2c8038a7f89e8a38f60ac640656f78R1-R3):
    Added a new file to export the theme module.
    *
    [`src/utils/theme/theme.js`](diffhunk://#diff-a64b8913370277e64be5780ab872f96593fd78d80161b8afde3393f0ece0a521R1-R22):
    Introduced a new theme module to define console output styles such as
    error, highlight, and strikethrough.

---
+++ src/utils/theme/theme.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview Console theme.
+ */
+
+// @ts-check
+
+// --------------------------------------------------------------------------------
+// Require
+// --------------------------------------------------------------------------------
+
+const c = require('ansi-colors');
+
+// --------------------------------------------------------------------------------
+// Exports
+// --------------------------------------------------------------------------------
+
+/** Console error theme. */
+module.exports.error = c.red.bold;
+/** Console highlight theme. */
+module.exports.highlight = c.white.bold;
+/** Console strikethrough theme. */
+module.exports.strikethrough = c.white.bold.strikethrough;
