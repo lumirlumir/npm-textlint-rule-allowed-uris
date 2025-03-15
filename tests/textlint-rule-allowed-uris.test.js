8d3a7e31378608010dfbec144853065040108821
루밀LuMir <rpfos@naver.com>
2025-03-16 02:08:05 +0900

    wip

--- tests/textlint-rule-allowed-uris.test.js
+++ tests/textlint-rule-allowed-uris.test.js
@@ -1,18 +1,34 @@
-const fs = require('node:fs');
-const path = require('node:path');
+/**
+ * @fileoverview Test for `textlint-rule-allowed-uris.js`.
+ */
+
+// --------------------------------------------------------------------------------
+// Require
+// --------------------------------------------------------------------------------
+
+const { readFileSync } = require('node:fs');
+const { resolve } = require('node:path');
+const { describe } = require('node:test');
+
 const TextLintTester = require('textlint-tester').default;
+
 const allowedUris = require('../src/textlint-rule-allowed-uris');
 const testCases = require('./textlint-rule-allowed-uris.data');
 
+// --------------------------------------------------------------------------------
+// Helpers
+// --------------------------------------------------------------------------------
+
 const tester = new TextLintTester();
-const testCasesMarkdown = fs.readFileSync(
-  path.resolve(__dirname, 'textlint-rule-allowed-uris.data.md'),
+const testCasesMarkdown = readFileSync(
+  resolve(__dirname, 'textlint-rule-allowed-uris.data.md'),
   'utf-8',
 );
 
-/**
- * Tests for the `textlint-rule-allowed-uris.js`
- */
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
 describe('textlint-rule-allowed-uris', () => {
   testCases.forEach(({ options, lines }) => {
     /* Initialization */
@@ -46,5 +62,13 @@ describe('textlint-rule-allowed-uris', () => {
         invalid,
       },
     );
+
+    console.log(`
+      allowed links: ${options?.allowed?.links?.join(' or ')}
+      allowed images: ${options?.allowed?.images?.join(' or ')}
+      disallowed links: ${options?.disallowed?.links?.join(' or ')}
+      disallowed images: ${options?.disallowed?.links?.join(' or ')}
+      lines: ${lines.join(', ')}
+      `);
   });
 });
