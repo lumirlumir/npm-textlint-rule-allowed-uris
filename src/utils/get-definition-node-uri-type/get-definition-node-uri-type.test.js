3183129a1e882eb68ccb8f54b22c1b485d5d9d1b
루밀LuMir <rpfos@naver.com>
2025-03-16 01:53:59 +0900

    wip

--- src/utils/get-definition-node-uri-type/get-definition-node-uri-type.test.js
+++ src/utils/get-definition-node-uri-type/get-definition-node-uri-type.test.js
@@ -1,15 +1,25 @@
-const assert = require('node:assert');
+/**
+ * @fileoverview Test for `get-definition-node-uri-type.js`.
+ */
+
+// --------------------------------------------------------------------------------
+// Require
+// --------------------------------------------------------------------------------
+
+const { strictEqual } = require('node:assert');
 const { describe, it } = require('node:test');
+
 const testCases = require('./get-definition-node-uri-type.data');
 const getDefinitionNodeUriType = require('./get-definition-node-uri-type');
 
-/**
- * Tests for the `getDefinitionNodeUriType.js`
- */
-describe('Util getDefinitionNodeUriType strictEqual testing', () => {
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+describe('get-definition-node-uri-type.js', () => {
   testCases.forEach(({ actual, expected }) => {
     it(`${actual} => ${expected}`, async () => {
-      assert.strictEqual(await getDefinitionNodeUriType(actual), expected);
+      strictEqual(await getDefinitionNodeUriType(actual), expected);
     });
   });
 });
--- src/utils/get-uri-types/get-uri-types.test.js
+++ src/utils/get-uri-types/get-uri-types.test.js
@@ -1,18 +1,28 @@
-const assert = require('node:assert');
+/**
+ * @fileoverview Test for `get-uri-types.js`.
+ */
+
+// --------------------------------------------------------------------------------
+// Require
+// --------------------------------------------------------------------------------
+
+const { deepStrictEqual, rejects } = require('node:assert');
 const { describe, it } = require('node:test');
+
 const testCases = require('./get-uri-types.data');
 const getUriTypes = require('./get-uri-types');
 
-/**
- * Tests for the `getUriTypes.js`
- */
-describe('Util getUriList deepStrictEqual and rejects testing', () => {
+// --------------------------------------------------------------------------------
+// Test
+// --------------------------------------------------------------------------------
+
+describe('get-uri-types.js', () => {
   testCases.forEach(({ actual, expected }) => {
     it(`${actual.raw} => ${expected.map(({ uri, type }) => `${uri} ${type}`).join(' / ')}`, async () => {
       if (['Link', 'Image', 'Definition', 'Html'].includes(actual.type))
-        assert.deepStrictEqual((await getUriTypes(actual)).uriTypes, expected);
+        deepStrictEqual((await getUriTypes(actual)).uriTypes, expected);
       else
-        await assert.rejects(async () => {
+        await rejects(async () => {
           await getUriTypes(actual);
         });
     });
