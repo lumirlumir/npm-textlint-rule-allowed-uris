eb9d47ebbe8dbf8d52fda26dad6193c96fcb5f7f
루밀LuMir <rpfos@naver.com>
2025-04-12 19:58:48 +0900

    wip

--- tests/textlint-rule-allowed-uris.test.js
+++ tests/textlint-rule-allowed-uris.test.js
@@ -34,7 +34,7 @@ const createErrors = lines => lines.map(line => ({ line }));
 
 test('textlint-rule-allowed-uris', () => {
   tester.run('textlint-rule-allowed-uris', rule, {
-    invalid: [
+    valid: [
       {
         name: 'Totally Empty - 1',
         text,
@@ -48,7 +48,190 @@ test('textlint-rule-allowed-uris', () => {
           //   images: [],
           // },
         },
-        errors: createErrors([]),
+      },
+      {
+        name: 'Totally Empty - 2',
+        text,
+        options: {
+          allowed: {
+            // links: [],
+            // images: [],
+          },
+          // disallowed: {
+          //   links: [],
+          //   images: [],
+          // },
+        },
+      },
+      {
+        name: 'Totally Empty - 3',
+        text,
+        options: {
+          // allowed: {
+          //   links: [],
+          //   images: [],
+          // },
+          disallowed: {
+            // links: [],
+            // images: [],
+          },
+        },
+      },
+      {
+        name: 'Totally Empty - 4',
+        text,
+        options: {
+          allowed: {
+            // links: [],
+            // images: [],
+          },
+          disallowed: {
+            // links: [],
+            // images: [],
+          },
+        },
+      },
+
+      {
+        name: 'Pass Empty Array - 1',
+        text,
+        options: {
+          allowed: {
+            // links: [],
+            // images: [],
+          },
+          disallowed: {
+            links: [], // Act like a blacklist. i.e. Empty array allows everything. No `links` will be detected.
+            // images: [],
+          },
+        },
+      },
+      {
+        name: 'Pass Empty Array - 2',
+        text,
+        options: {
+          allowed: {
+            // links: [],
+            // images: [],
+          },
+          disallowed: {
+            // links: [],
+            images: [], // Act like a blacklist. i.e. Empty array allows everything. No `images` will be detected.
+          },
+        },
+      },
+      {
+        name: 'Pass Empty Array - 3',
+        text,
+        options: {
+          allowed: {
+            // links: [],
+            // images: [],
+          },
+          disallowed: {
+            links: [], // Act like a blacklist. i.e. Empty array allows everything. No `images` will be detected.
+            images: [], // Act like a blacklist. i.e. Empty array allows everything. No `images` will be detected.
+          },
+        },
+      },
+    ],
+
+    invalid: [
+      {
+        name: 'Pass Empty Array - 1',
+        text,
+        options: {
+          allowed: {
+            links: [], // Act like an whitelist. i.e. Empty array allows nothing. Every `links` will be detected.
+            // images: [],
+          },
+          disallowed: {
+            // links: [],
+            // images: [],
+          },
+        },
+        errors: createErrors([
+          7, 9, 13, 15, 19, 21, 23, 25, 31, 37, 43, 47, 86, 101, 103, 105,
+        ]),
+      },
+      {
+        name: 'Pass Empty Array - 2',
+        text,
+        options: {
+          allowed: {
+            // links: [],
+            images: [], // Act like an whitelist. i.e. Empty array allows nothing. Every `images` will be detected.
+          },
+          disallowed: {
+            // links: [],
+            // images: [],
+          },
+        },
+        errors: createErrors([53, 59, 65, 71, 75, 77, 79, 86, 105]),
+      },
+      {
+        name: 'Pass Empty Array - 3',
+        text,
+        options: {
+          allowed: {
+            links: [], // Act like an whitelist. i.e. Empty array allows nothing. Every `links` will be detected.
+            images: [], // Act like an whitelist. i.e. Empty array allows nothing. Every `images` will be detected.
+          },
+          disallowed: {
+            // links: [],
+            // images: [],
+          },
+        },
+        errors: createErrors([
+          7, 9, 13, 15, 19, 21, 23, 25, 31, 37, 43, 47, 53, 59, 65, 71, 75, 77, 79, 86,
+          86, 101, 103, 105, 105,
+        ]), // `86` and `105` are same line number, but detected twice. one for `links` and the other for `images`
+      },
+
+      {
+        name: 'Pass Values - `allowed.links` - 1',
+        text,
+        options: {
+          allowed: {
+            links: [/google/],
+            // images: [],
+          },
+          // disallowed: {
+          //   links: [],
+          //   images: [],
+          // },
+        },
+        errors: createErrors([9, 15, 21, 23, 25, 86, 101, 103]),
+      },
+      {
+        name: 'Pass Values - `allowed.links` - 2',
+        text,
+        options: {
+          allowed: {
+            links: [/google/, /gmail/],
+            // images: [],
+          },
+          // disallowed: {
+          //   links: [],
+          //   images: [],
+          // },
+        },
+        errors: createErrors([21, 23, 25, 86, 101, 103]),
+      },
+      {
+        name: 'Pass Values - `allowed.links` - 3',
+        text,
+        options: {
+          allowed: {
+            links: [/google/, /gmail/, /README.md/],
+            // images: [],
+          },
+          // disallowed: {
+          //   links: [],
+          //   images: [],
+          // },
+        },
+        errors: createErrors([21, 23, 86, 103]),
       },
     ],
   });
