80f61bc44755890f5cb8f57e94892b6ae0629e32
루밀LuMir <rpfos@naver.com>
2025-07-17 18:51:28 +0900

    refactor(*): improve structure and clarity of URI type retrieval functions

--- src/utils/get-uri-types/get-uri-types.js
+++ src/utils/get-uri-types/get-uri-types.js
@@ -26,32 +26,35 @@ const getDefinitionNodeUriType = require('../get-definition-node-uri-type');
 /**
  * Retrieves URI from a given `Link` node and returns an instance of `UriTypes`.
  * @param {TxtLinkNode} node `Link` type node.
+ * @return {UriTypes}
  */
 const getUriTypesLink = ({ url }) => new UriTypes().push({ uri: url, type: 'link' });
 
 /**
  * Retrieves URI from a given `Image` node and returns an instance of `UriTypes`.
  * @param {TxtImageNode} node `Image` type node.
+ * @return {UriTypes}
  */
 const getUriTypesImage = ({ url }) => new UriTypes().push({ uri: url, type: 'image' });
 
 /**
  * Retrieves URI from a given `Definition` node and returns an instance of `UriTypes`.
  * @param {TxtDefinitionNode} node `Definition` type node.
+ * @return {Promise<UriTypes>}
  * @async
  */
 const getUriTypesDefinition = async ({ url }) => {
   const type = await getDefinitionNodeUriType(url);
 
-  return ['link', 'image'].includes(type)
-    ? // @ts-ignore -- TODO
-      new UriTypes().push({ uri: url, type })
+  return type === 'link' || type === 'image'
+    ? new UriTypes().push({ uri: url, type })
     : new UriTypes();
 };
 
 /**
  * Parses the HTML content of the given node and retrieves all the `<a>` and `<img>` tag's URIs.
  * @param {TxtHtmlNode} node `Html` type node.
+ * @return {UriTypes}
  */
 const getUriTypesHtml = ({ value }) => {
   const uriTypes = new UriTypes();
@@ -80,22 +83,9 @@ const getUriTypesHtml = ({ value }) => {
 // Exports
 // --------------------------------------------------------------------------------
 
-/**
- * Retrieves the URI and creates an instance of `UriTypes` from a given `node`.
- * @param {TxtLinkNode | TxtImageNode | TxtDefinitionNode | TxtHtmlNode} node The node from which to retrieve the URI.
- * @async
- */
-module.exports = async function getUriTypes(node) {
-  switch (node.type) {
-    case 'Link':
-      return getUriTypesLink(node);
-    case 'Image':
-      return getUriTypesImage(node);
-    case 'Definition':
-      return getUriTypesDefinition(node);
-    case 'Html':
-      return getUriTypesHtml(node);
-    default: // Unreachable
-      return undefined;
-  }
+module.exports = {
+  getUriTypesLink,
+  getUriTypesImage,
+  getUriTypesDefinition,
+  getUriTypesHtml,
 };
