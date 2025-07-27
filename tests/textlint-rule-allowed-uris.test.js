55381208506929e32e81ed95ae51b3c66b1ae4dc
루밀LuMir <rpfos@naver.com>
2025-07-27 17:03:22 +0900

    wip

--- tests/textlint-rule-allowed-uris.test.js
+++ tests/textlint-rule-allowed-uris.test.js
@@ -1,5 +1,5 @@
 /**
- * @fileoverview Test for `textlint-rule-allowed-uris.js`.
+ * @fileoverview Test for `textlint-rule-allowed-uris.ts`.
  */
 
 // --------------------------------------------------------------------------------
@@ -11,7 +11,7 @@ import { readFileSync } from 'node:fs';
 import { resolve } from 'node:path';
 import { test } from 'node:test';
 
-import rule from '../src/textlint-rule-allowed-uris.js';
+import rule from '../build/textlint-rule-allowed-uris.js';
 
 const TextLintTester = createRequire(import.meta.url)('textlint-tester').default;
 
@@ -149,6 +149,11 @@ test('textlint-rule-allowed-uris', () => {
           // },
         },
       },
+
+      {
+        name: 'Do not report unused definitions by default',
+        text: '[link1]: https://www.google.com',
+      },
     ],
 
     invalid: [
@@ -167,16 +172,56 @@ test('textlint-rule-allowed-uris', () => {
         },
         errors: [
           {
-            line: 7,
             range: [97, 119],
+            loc: {
+              start: { line: 7, column: 1 },
+              end: { line: 7, column: 23 },
+            },
           },
           {
-            line: 9,
             range: [121, 138],
+            loc: {
+              start: { line: 9, column: 1 },
+              end: { line: 9, column: 18 },
+            },
+          },
+          ...createErrors([13, 15, 19, 21, 23, 25, 31, 37, 43, 47, 86, 101, 105]),
+        ],
+      },
+
+      {
+        name: 'Pass Empty Array - 1 (`checkUnusedDefinitions`: true)',
+        text,
+        options: {
+          allowed: {
+            links: [], // Act like an whitelist. i.e. Empty array allows nothing. Every `links` will be detected.
+            // images: [],
+          },
+          disallowed: {
+            // links: [],
+            // images: [],
+          },
+          checkUnusedDefinitions: true,
+        },
+        errors: [
+          {
+            range: [97, 119],
+            loc: {
+              start: { line: 7, column: 1 },
+              end: { line: 7, column: 23 },
+            },
+          },
+          {
+            range: [121, 138],
+            loc: {
+              start: { line: 9, column: 1 },
+              end: { line: 9, column: 18 },
+            },
           },
           ...createErrors([13, 15, 19, 21, 23, 25, 31, 37, 43, 47, 86, 101, 103, 105]),
         ],
       },
+
       {
         name: 'Pass Empty Array - 2',
         text,
@@ -192,6 +237,7 @@ test('textlint-rule-allowed-uris', () => {
         },
         errors: createErrors([53, 59, 65, 71, 75, 77, 79, 86, 105]),
       },
+
       {
         name: 'Pass Empty Array - 3',
         text,
@@ -204,6 +250,26 @@ test('textlint-rule-allowed-uris', () => {
             // links: [],
             // images: [],
           },
+        },
+        errors: createErrors([
+          7, 9, 13, 15, 19, 21, 23, 25, 31, 37, 43, 47, 53, 59, 65, 71, 75, 77, 79, 86,
+          86, 101, 105, 105,
+        ]), // `86` and `105` are same line number, but detected twice. one for `links` and the other for `images`
+      },
+
+      {
+        name: 'Pass Empty Array - 3 (`checkUnusedDefinitions`: true)',
+        text,
+        options: {
+          allowed: {
+            links: [], // Act like an whitelist. i.e. Empty array allows nothing. Every `links` will be detected.
+            images: [], // Act like an whitelist. i.e. Empty array allows nothing. Every `images` will be detected.
+          },
+          disallowed: {
+            // links: [],
+            // images: [],
+          },
+          checkUnusedDefinitions: true,
         },
         errors: createErrors([
           7, 9, 13, 15, 19, 21, 23, 25, 31, 37, 43, 47, 53, 59, 65, 71, 75, 77, 79, 86,
@@ -224,8 +290,26 @@ test('textlint-rule-allowed-uris', () => {
           //   images: [],
           // },
         },
+        errors: createErrors([9, 15, 21, 23, 25, 86, 101]),
+      },
+
+      {
+        name: 'Pass Values - `allowed.links` - 1 (`checkUnusedDefinitions`: true)',
+        text,
+        options: {
+          allowed: {
+            links: [/google/],
+            // images: [],
+          },
+          // disallowed: {
+          //   links: [],
+          //   images: [],
+          // },
+          checkUnusedDefinitions: true,
+        },
         errors: createErrors([9, 15, 21, 23, 25, 86, 101, 103]),
       },
+
       {
         name: 'Pass Values - `allowed.links` - 2',
         text,
@@ -239,8 +323,26 @@ test('textlint-rule-allowed-uris', () => {
           //   images: [],
           // },
         },
+        errors: createErrors([21, 23, 25, 86, 101]),
+      },
+
+      {
+        name: 'Pass Values - `allowed.links` - 2 (`checkUnusedDefinitions`: true)',
+        text,
+        options: {
+          allowed: {
+            links: [/google/, /gmail/],
+            // images: [],
+          },
+          // disallowed: {
+          //   links: [],
+          //   images: [],
+          // },
+          checkUnusedDefinitions: true,
+        },
         errors: createErrors([21, 23, 25, 86, 101, 103]),
       },
+
       {
         name: 'Pass Values - `allowed.links` - 3',
         text,
@@ -253,6 +355,23 @@ test('textlint-rule-allowed-uris', () => {
           //   links: [],
           //   images: [],
           // },
+        },
+        errors: createErrors([21, 23, 86]),
+      },
+
+      {
+        name: 'Pass Values - `allowed.links` - 3 (`checkUnusedDefinitions`: true)',
+        text,
+        options: {
+          allowed: {
+            links: [/google/, /gmail/, /README.md/],
+            // images: [],
+          },
+          // disallowed: {
+          //   links: [],
+          //   images: [],
+          // },
+          checkUnusedDefinitions: true,
         },
         errors: createErrors([21, 23, 86, 103]),
       },
@@ -272,6 +391,24 @@ test('textlint-rule-allowed-uris', () => {
         },
         errors: createErrors([7, 13, 19, 31, 37, 43, 47, 105]),
       },
+
+      {
+        name: 'Pass Values - `disallowed.links` - 1 (`checkUnusedDefinitions`: true)',
+        text,
+        options: {
+          // allowed: {
+          //   links: [],
+          //   images: [],
+          // },
+          disallowed: {
+            links: [/google/],
+            // images: [],
+          },
+          checkUnusedDefinitions: true,
+        },
+        errors: createErrors([7, 13, 19, 31, 37, 43, 47, 103, 105]),
+      },
+
       {
         name: 'Pass Values - `disallowed.links` - 2',
         text,
@@ -287,6 +424,7 @@ test('textlint-rule-allowed-uris', () => {
         },
         errors: createErrors([7, 9, 13, 15, 19, 31, 37, 43, 47, 105]),
       },
+
       {
         name: 'Pass Values - `disallowed.links` - 3',
         text,
@@ -333,9 +471,29 @@ test('textlint-rule-allowed-uris', () => {
           },
         },
         errors: createErrors([
+          7, 9, 13, 15, 19, 21, 23, 25, 31, 37, 43, 47, 86, 101, 105,
+        ]), // Every `links` will be detected.
+      },
+
+      {
+        name: 'Pass same values to `allowed` and `disallowed` - 1 (`checkUnusedDefinitions`: true)',
+        text,
+        options: {
+          allowed: {
+            links: [/google/],
+            // images: [],
+          },
+          disallowed: {
+            links: [/google/],
+            // images: [],
+          },
+          checkUnusedDefinitions: true,
+        },
+        errors: createErrors([
           7, 9, 13, 15, 19, 21, 23, 25, 31, 37, 43, 47, 86, 101, 103, 105,
         ]), // Every `links` will be detected.
       },
+
       {
         name: 'Pass same values to `allowed` and `disallowed` - 2',
         text,
@@ -350,6 +508,20 @@ test('textlint-rule-allowed-uris', () => {
           },
         },
         errors: createErrors([53, 59, 65, 71, 75, 77, 79, 86, 105]), // Every `images` will be detected.
+      },
+
+      {
+        name: 'Report unused definitions when `checkUnusedDefinitions` is true',
+        text: '[link1]: https://www.google.com',
+        options: {
+          checkUnusedDefinitions: true,
+        },
+        errors: [
+          {
+            line: 1,
+            range: [0, 31],
+          },
+        ],
       },
     ],
   });
